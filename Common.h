86d1b10fe2508afdbcaae6ea93f776fb1d0d4637
Student <student@lab-315-029.cs.csubak.edu>
2018-02-23 09:17:55 -0800

    Merged all of our files. Im a god.

---
+++ Common.h
@@ -0,0 +1,14 @@
+#ifndef _COMMON_HEADERS_
+#define _COMMON_HEADERS_
+
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+
+/*
+x11 and opengl gose here
+
+just to keep main.cpp clean
+*/
+
+#endif
