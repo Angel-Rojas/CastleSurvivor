e7309f79b28b5ba01965257ddf52b350075cb136
Christy <christy1392@hotmail.com>
2018-02-24 18:29:53 -0800

    Add files via upload

---
+++ christy.cpp
@@ -0,0 +1,25 @@
+
+
+//Created by christy guerrero
+//christy.cpp
+
+//#include "Common.h"
+#include "christy.h"
+//#include "fonts.h"
+//#include <GL/glx.h>
+//#include <X11/Xlib.h>
+#include <iostream>
+using namespace std;
+
+void printName(){
+	/*extern Rect c;
+	glClear(GL_COLOR_BUFFER_BIT);
+	//
+	c.bot = 900 - 20;
+	c.left = 15;
+	c.center = 0;
+	ggprint8b(&c, 16, 0x00ff0000, "Christy");	*/
+	cout << "Christy";
+	return;
+}
+
---
+++ christy.h
@@ -0,0 +1,7 @@
+//christy's header file and functions
+#ifndef _CHRISTY_STUFF_
+#define _CHRISTY_STUFF_
+#include "Common.h"
+void printName();
+
+#endif
