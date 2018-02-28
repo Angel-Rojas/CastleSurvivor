ae9354c46e1caf367d9dcb195cae5afeec9a8221
Abdullah_Aljahdali <Alja7dali@gmail.com>
2018-02-28 08:31:37 -0800

    Button struct func update!

--- abdullahA.cpp
+++ abdullahA.cpp
@@ -4,12 +4,20 @@ by: Abdullah Aljahdali
 
 
 Button *makeButton(char *title,
+                       float x,
+                       float y,
                        float w,
                        float h,
   void (*action)(float, float)) {
     Button *b = (Button*)malloc(sizeof(Button));
     strcpy(title, b->title);
+    b->x = x;
+    b->y = y;
     b->width = w;
     b->height = h;
     b->action = action;
+}
+
+int isPointerInsideButton(float x, float y, Button *b) {
+  return (b->x < x && x < b->x+b->w && b->y < y && y < b->y+b->h);
 }
--- abdullahA.h
+++ abdullahA.h
@@ -10,9 +10,11 @@ struct Button {
   char *title;
   float width;
   float height;
+  float x;
+  float y;
   void (*action)(float, float):
 } typedef struct Button Button;
 
-Button *makeButton(char *title, float w, float h, void (*action)(float, float));
-
+Button *makeButton(char *title, float x, float y, float w, float h, void (*action)(float, float));
+int isPointerInsideButton(float x, float y, Button *b);
 #endif
